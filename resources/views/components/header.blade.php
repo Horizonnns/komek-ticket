ce181f0a469ab519e823e6cb46048230fe54a6a9
horizonnns <shadowcrewhack@gmail.com>
2025-12-23 18:27:56 +0500

    Added a background image and main slider

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
 <header class="header">
     <div class="container">
         <nav class="header__nav">
-            <img src="{{ asset('images/logo.svg') }}" alt="Logo">
+            <img src="{{ asset('/public/main-slide.svg') }}" alt="slide">
             </nav>
     </div>
 </header>
