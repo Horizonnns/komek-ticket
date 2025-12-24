ed5e5bc495ae2b5b3987019fa4b3292ccb11c471
horizonnns <shadowcrewhack@gmail.com>
2025-12-24 16:21:30 +0500

    feat: implement footer component with adding info and updated styling

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,24 @@
 <footer class="footer">
-    <div class="container">
-        <div class="footer__nav">
-            <!-- <img src="{{ asset('images/logo.svg') }}" alt="Logo"> -->
-            </div>
+  <div class="footer__container">
+    <div class="footer__logo">
+      <img src="{{ asset('images/icons/logo/komek-logo.svg') }}" alt="Komek By Ticket">
     </div>
+
+    <div class="footer__info">
+      <a href="mailto:admin@example.com" class="footer__info-title">admin@example.com</a>
+      <a href="mailto:ticket@example.com" class="footer__info-title">ticket@example.com</a>
+
+      <span class="footer__info-sub">улица Тестовая 1</span>
+    </div>
+
+    <div class="footer__info">
+      <a href="tel:+77750000001" class="footer__info-title">+7 (775) 000 00 01</a>
+      <span class="footer__info-sub">Касса</span>
+    </div>
+
+    <div class="footer__info">
+      <span class="footer__info-title">08:00 - 22:00 с пн.-пт.</span>
+      <span class="footer__info-sub">График работы</span>
+    </div>
+  </div>
 </footer>
