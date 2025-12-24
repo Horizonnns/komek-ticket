9a88deb32dfa910cb2909028784e91346d3376d9
horizonnns <shadowcrewhack@gmail.com>
2025-12-24 14:32:29 +0500

    refactor: Fixed container styles, did some minor refactoring

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <div class="footer__nav">
-            <img src="{{ asset('images/logo.svg') }}" alt="Logo">
+            <!-- <img src="{{ asset('images/logo.svg') }}" alt="Logo"> -->
             </div>
     </div>
 </footer>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,5 @@
 <header class="header">
-    <div class="container">
         <nav class="header__nav">
             <img src="{{ asset('images/slides/main-slide.svg') }}" alt="slide">
             </nav>
-    </div>
 </header>
