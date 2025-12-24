53fa4e2a4d0c77403f1ff25a28425af8d3ee9fe8
horizonnns <shadowcrewhack@gmail.com>
2025-12-24 15:33:08 +0500

    feat: implement header component with social media icons and updated styling

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,35 @@
 <header class="header">
-        <nav class="header__nav">
-            <img src="{{ asset('images/slides/main-slide.svg') }}" alt="slide">
-            </nav>
+  <div class="header__slide">
+       <img src="{{ asset('images/slides/main-slide.svg') }}" alt="Slide">
+  </div>
+
+    <div class="header__container">
+        <div class="header__logo">
+            <img src="{{ asset('images/icons/logo/komek-logo.svg') }}" alt="Komek By Ticket">
+        </div>
+
+        <div class="header__info">
+            <span class="header__info-title">Казахстан</span>
+            <span class="header__info-sub">улица Тестовая 1</span>
+        </div>
+
+        <div class="header__info">
+            <a href="tel:+77750000001" class="header__info-title">+7 (775) 000 00 01</a>
+            <span class="header__info-sub">Касса</span>
+        </div>
+
+        <div class="header__actions">
+            <div class="header__socials">
+                <a href="#"><img src="{{ asset('images/icons/facebook.svg') }}" alt="FB"></a>
+                <a href="#"><img src="{{ asset('images/icons/instagram.svg') }}" alt="IG"></a>
+                <a href="#"><img src="{{ asset('images/icons/youtube.svg') }}" alt="YT"></a>
+                <a href="#"><img src="{{ asset('images/icons/vk.svg') }}" alt="VK"></a>
+            </div>
+
+            <button class="header__btn">
+                Мне пришёл билет
+            </button>
+        </div>
+
+    </div>
 </header>
