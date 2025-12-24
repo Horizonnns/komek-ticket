16aa595d3502fd66ba302221f4b5e4b6b51ec03e
horizonnns <shadowcrewhack@gmail.com>
2025-12-24 19:46:18 +0500

    style: reduce the height of the header swiper wrapper from 450px to 400px and correct the formatting of the data array

--- routes/web.php
+++ routes/web.php
@@ -3,172 +3,172 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $tickets = [
+  $tickets = [
+    [
+      'id' => 1,
+      'title' => 'Праздники',
+      "genre" => ["комедия"],
+      'poster' => 'posters/prazdniki.svg',
+      "premiere" => true,
+      "age_rating" => "12+",
+      "places" => [
         [
-            'id' => 1,
-            'title' => 'Праздники',
-            "genre" => ["комедия"],
-            'poster' => 'posters/prazdniki.svg',
-            "premiere" => true,
-            "age_rating" => "12+",
-            "places" => [
-                [
-                    'time' => '15:35',
-                    'type' => '2D',
-                    'price' => 1233,
-                    'date' => '25.12.2025',
-                    'hall' => '3'
-                ],
-                [
-                    'time' => '17:40',
-                    'type' => '2D',
-                    'price' => 3080,
-                    'date' => '25.12.2025',
-                    'hall' => '6 VIP'
-                ],
-                [
-                    'time' => '19:15',
-                    'type' => '2D',
-                    'price' => 1700,
-                    'date' => '25.12.2025',
-                    'hall' => '4'
-                ],
-                [
-                    'time' => '21:05',
-                    'type' => '2D',
-                    'price' => 1700,
-                    'date' => '25.12.2025',
-                    'hall' => '3'
-                ],
-                [
-                    'time' => '21:40',
-                    'type' => '2D',
-                    'price' => 4030,
-                    'date' => '25.12.2025',
-                    'hall' => '6 VIP'
-                ],
-                [
-                    'time' => '22:45',
-                    'type' => '2D',
-                    'price' => 1700,
-                    'date' => '25.12.2025',
-                    'hall' => '3'
-                ],
-            ],
+          'time' => '15:35',
+          'type' => '2D',
+          'price' => 1233,
+          'date' => '25.12.2025',
+          'hall' => '3'
         ],
         [
-            'id' => 2,
-            'title' => 'Мег 2: Бездна',
-            "genre" => ["экшен", "триллер"],
-            'poster' => 'posters/meg2.svg',
-            "premiere" => false,
-            "age_rating" => "16+",
-            "places" => [
-                [
-                    'time' => '15:35',
-                    'type' => '2D',
-                    'price' => 4030,
-                    'date' => '25.12.2025',
-                    'hall' => '6 VIP'
-                ],
-                [
-                    'time' => '17:40',
-                    'type' => '2D',
-                    'price' => 3080,
-                    'date' => '25.12.2025',
-                    'hall' => '2 Auro'
-                ],
-                [
-                    'time' => '19:15',
-                    'type' => '2D',
-                    'price' => 1700,
-                    'date' => '25.12.2025',
-                    'hall' => '4'
-                ],
-            ],
+          'time' => '17:40',
+          'type' => '2D',
+          'price' => 3080,
+          'date' => '25.12.2025',
+          'hall' => '6 VIP'
         ],
         [
-            'id' => 3,
-            'title' => 'Заложники',
-            "genre" => ["экшен", "триллер"],
-            'poster' => 'posters/zalozhniky.svg',
-            "premiere" => false,
-            "age_rating" => "16+",
-            "places" => [
-                [
-                    'time' => '16:10',
-                    'type' => '2D',
-                    'price' => 4030,
-                    'date' => '25.12.2025',
-                    'hall' => '1'
-                ],
-                [
-                    'time' => '21:30',
-                    'type' => '2D',
-                    'price' => 3080,
-                    'date' => '25.12.2025',
-                    'hall' => '1'
-                ],
-            ],
+          'time' => '19:15',
+          'type' => '2D',
+          'price' => 1700,
+          'date' => '25.12.2025',
+          'hall' => '4'
         ],
         [
-            'id' => 4,
-            'title' => 'Леди Баг и Супер-Кот: Пробуждение силы',
-            "genre" => ["фентези", "боевик", "мелодрама", "мультфильм", "комедия"],
-            'poster' => 'posters/ledi-bag.svg',
-            "premiere" => false,
-            "age_rating" => "6+",
-            "places" => [
-                [
-                    'time' => '16:10',
-                    'type' => '2D',
-                    'price' => 4030,
-                    'date' => '25.12.2025',
-                    'hall' => '1'
-                ],
-                [
-                    'time' => '21:30',
-                    'type' => '2D',
-                    'price' => 3080,
-                    'date' => '25.12.2025',
-                    'hall' => '1'
-                ],
-            ],
+          'time' => '21:05',
+          'type' => '2D',
+          'price' => 1700,
+          'date' => '25.12.2025',
+          'hall' => '3'
         ],
         [
-            'id' => 5,
-            'title' => 'Когда не загрузилась афиша',
-            "genre" => ["комедия"],
-            'poster' => 'posters/afisha-nezagruzilas.svg',
-            "premiere" => true,
-            // "age_rating" => "12+",
-            "places" => [
-                [
-                    'time' => '15:35',
-                    'type' => '2D',
-                    'price' => 1233,
-                    'date' => '25.12.2025',
-                    'hall' => '3'
-                ],
-                [
-                    'time' => '17:40',
-                    'type' => '2D',
-                    'price' => 3080,
-                    'date' => '25.12.2025',
-                    'hall' => '6 VIP'
-                ],
-            ],
+          'time' => '21:40',
+          'type' => '2D',
+          'price' => 4030,
+          'date' => '25.12.2025',
+          'hall' => '6 VIP'
         ],
         [
-            'id' => 6,
-            'title' => 'Руслан и Людмила. Больше, чем сказка',
-            "genre" => ["анимационое", "приключение"],
-            'poster' => 'posters/ruslan-i-ludmila.svg',
-            "premiere" => true,
-            "age_rating" => "6+",
-            'sessions' => 'Сеансы на Завтра'
+          'time' => '22:45',
+          'type' => '2D',
+          'price' => 1700,
+          'date' => '25.12.2025',
+          'hall' => '3'
         ],
-    ];
+      ],
+    ],
+    [
+      'id' => 2,
+      'title' => 'Мег 2: Бездна',
+      "genre" => ["экшен", "триллер"],
+      'poster' => 'posters/meg2.svg',
+      "premiere" => false,
+      "age_rating" => "16+",
+      "places" => [
+        [
+          'time' => '15:35',
+          'type' => '2D',
+          'price' => 4030,
+          'date' => '25.12.2025',
+          'hall' => '6 VIP'
+        ],
+        [
+          'time' => '17:40',
+          'type' => '2D',
+          'price' => 3080,
+          'date' => '25.12.2025',
+          'hall' => '2 Auro'
+        ],
+        [
+          'time' => '19:15',
+          'type' => '2D',
+          'price' => 1700,
+          'date' => '25.12.2025',
+          'hall' => '4'
+        ],
+      ],
+    ],
+    [
+      'id' => 3,
+      'title' => 'Заложники',
+      "genre" => ["экшен", "триллер"],
+      'poster' => 'posters/zalozhniky.svg',
+      "premiere" => false,
+      "age_rating" => "16+",
+      "places" => [
+        [
+          'time' => '16:10',
+          'type' => '2D',
+          'price' => 4030,
+          'date' => '25.12.2025',
+          'hall' => '1'
+        ],
+        [
+          'time' => '21:30',
+          'type' => '2D',
+          'price' => 3080,
+          'date' => '25.12.2025',
+          'hall' => '1'
+        ],
+      ],
+    ],
+    [
+      'id' => 4,
+      'title' => 'Леди Баг и Супер-Кот: Пробуждение силы',
+      "genre" => ["фентези", "боевик", "мелодрама", "мультфильм", "комедия"],
+      'poster' => 'posters/ledi-bag.svg',
+      "premiere" => false,
+      "age_rating" => "6+",
+      "places" => [
+        [
+          'time' => '16:10',
+          'type' => '2D',
+          'price' => 4030,
+          'date' => '25.12.2025',
+          'hall' => '1'
+        ],
+        [
+          'time' => '21:30',
+          'type' => '2D',
+          'price' => 3080,
+          'date' => '25.12.2025',
+          'hall' => '1'
+        ],
+      ],
+    ],
+    [
+      'id' => 5,
+      'title' => 'Когда не загрузилась афиша',
+      "genre" => ["комедия"],
+      'poster' => 'posters/afisha-nezagruzilas.svg',
+      "premiere" => true,
+      // "age_rating" => "12+",
+      "places" => [
+        [
+          'time' => '15:35',
+          'type' => '2D',
+          'price' => 1233,
+          'date' => '25.12.2025',
+          'hall' => '3'
+        ],
+        [
+          'time' => '17:40',
+          'type' => '2D',
+          'price' => 3080,
+          'date' => '25.12.2025',
+          'hall' => '6 VIP'
+        ],
+      ],
+    ],
+    [
+      'id' => 6,
+      'title' => 'Руслан и Людмила. Больше, чем сказка',
+      "genre" => ["анимационое", "приключение"],
+      'poster' => 'posters/ruslan-i-ludmila.svg',
+      "premiere" => true,
+      "age_rating" => "6+",
+      'sessions' => 'Сеансы на Завтра'
+    ],
+  ];
 
-    return view('welcome', compact('tickets'));
+  return view('welcome', compact('tickets'));
 });
