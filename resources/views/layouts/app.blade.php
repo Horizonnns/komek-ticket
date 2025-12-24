03e2edd4bbfccef77a7e3314ee5500a95bf777f2
horizonnns <shadowcrewhack@gmail.com>
2025-12-25 01:14:18 +0500

    feat: Implemented controls section for filtering movie cards

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Komek By Ticket - @yield('title')</title>
 
-    @vite(['resources/css/app.scss', 'resources/js/app.ts'])
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Komek By Ticket - @yield('title')</title>
+
+  @vite(['resources/css/app.scss', 'resources/js/app.ts'])
 </head>
 
 <body>
-    <div id="app">
-        <x-header />
+  <div id="app">
+    <x-header />
 
-        <main>
-            @yield('content')
-        </main>
+    <main>
+      @yield('content')
+    </main>
 
-        <x-footer />
-    </div>
+    <x-footer />
+  </div>
 </body>
+
 </html>
