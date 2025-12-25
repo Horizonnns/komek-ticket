eff226427b1d6a23582d34181b47525b17b8c118
horizonnns <shadowcrewhack@gmail.com>
2025-12-25 18:09:03 +0500

    feat(pwa): Implement stale-while-revalidate caching in sw.js for instant loading and seamless background updates

--- public/sw.js
+++ public/sw.js
@@ -16,8 +16,17 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.match(event.request).then((cachedResponse) => {
+                const fetchedResponse = fetch(event.request).then(
+                    (networkResponse) => {
+                        cache.put(event.request, networkResponse.clone());
+                        return networkResponse;
+                    }
+                );
+
+                return cachedResponse || fetchedResponse;
+            });
         })
     );
 });
